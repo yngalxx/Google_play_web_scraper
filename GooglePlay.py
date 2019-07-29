eb3cde6b142bdb217571936f8e1c3f030a1c1637
Alex Drożdż <45334449+yngalxx@users.noreply.github.com>
2019-07-29 18:12:21 +0200

    Update GooglePlay.py

--- GooglePlay.py
+++ GooglePlay.py
@@ -159,11 +159,11 @@ for z in tqdm(range(z), ncols=80):
             filewriter.writerow([single_date, single_rating, single_content, single_usefulness])
 
         # Google Play load next reviews only if you scroll down
-        browser.execute_script("window.scrollBy(0, 90)")
+        browser.execute_script("window.scrollBy(0, document.body.scrollHeight)")
 
         # Try click button if it is on page, if not go forward
         try:
-            browser.find_element_by_xpath('/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div[2]/div/span').click()
+            browser.find_element_by_css_selector('.PFAhAf > div:nth-child(1) > span:nth-child(3) > span:nth-child(1)').click()
 
         except:
             pass
